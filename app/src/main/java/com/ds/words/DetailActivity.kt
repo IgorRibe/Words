159adb1e7e6a9d941987b7118570084584ffaa3f
IgorRibe <rc.igorr@gmail.com>
2022-06-15 22:41:00 -0300

    Criando words

--- app/src/main/java/com/ds/words/DetailActivity.kt
+++ app/src/main/java/com/ds/words/DetailActivity.kt
@@ -1,11 +1,34 @@
 package com.ds.words
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ds.words.databinding.ActivityDetailBinding
+
 
 class DetailActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val letterId = "A"
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordAdapter(letterId, this)
+
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+
+        title = getString(R.string.detaidl_prefix) + " " + letterId
     }
 }
+
